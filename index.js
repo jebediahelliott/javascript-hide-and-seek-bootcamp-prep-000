4dff62abc98dc14bb7523903e22e71de98e7e9c2
Jebediah Elliott <jebediahelliott@gmail.com>
2018-05-05 21:30:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ function getFirstSelector(selector)
 }
 function nestedTarget()
 {
-  var id = document.getElementById('nested')
+  var id = document.getElementById('nested');
   return id.querySelector('.target');
 }
 function increaseRankBy(n)
 {
-  var lists = document.getElementsByClass('.ranked-')
+  var lists = document.getElementsByClassName('.ranked-list');
+  for(let i = 0; i < lists.length; i++)
+  {
+    lists[i] = lists[i] + n;
+  }
 }
