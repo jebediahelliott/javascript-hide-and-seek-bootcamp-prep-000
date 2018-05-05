8bcca744515b4593914a9dd16c6a94ad1fef1536
Jebediah Elliott <jebediahelliott@gmail.com>
2018-05-05 22:25:38 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function getFirstSelector(selector)
+{
+  return document.querySelector(selector);
+}
+function nestedTarget()
+{
+  var id = document.getElementById('nested');
+  return id.querySelector('.target');
+}
+function increaseRankBy(n)
+{
+  const lists = document.querySelectorAll('.ranked-list');
+  for(let i = 0; i < lists.length; i++)
+  {
+    parseInt(lists[i].innerHTML);
+    lists[i].innerHTML = lists[i] + n;
+  }
+}
+function deepestChild()
+{
+  let finder = document.getElementById('grand-node').querySelectorAll('div');
+  return finder[finder.length - 1];
+}
